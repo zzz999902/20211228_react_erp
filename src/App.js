f4eee341e9627004d90bfa1e0d0d661335dff99e
e <l>
2021-12-28 16:12:43 +0800

    init app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+         React
+      </header>
+    </div>
+  );
+}
+
+export default App;
